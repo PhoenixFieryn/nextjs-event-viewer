9b54b722f0a66e408f6ed6cee8827069545731d5
Austin Hou <austin@austinhou.com>
2021-05-28 11:47:40 -0700

    Added events search component without logic

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,15 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list/event-list.component';
+import EventsSearch from '../../components/events/events-search/events-search.component';
+
 function AllEventsPage() {
+	const events = getAllEvents();
+
 	return (
-		<div>
-			<h1>All Events</h1>
-		</div>
+		<>
+			<EventsSearch />
+			<EventList items={events} />
+		</>
 	);
 }
 
